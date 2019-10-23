19a1d6c9b300c91cc945cae3992ffe185f4e89bf
Sam Lapusniak <samlapusniak@uvic.ca>
2019-10-23 10:46:42 -0700

    first upload pls be nice ;)

---
+++ dracojss.js
@@ -0,0 +1,7 @@
+function popup(message){
+    /* my parameters go inside the regular brackets 
+    all my function code goes between the curly bracket*/
+ alert(message); 
+ var dracoclick=document.getElementById("dragongif");
+ dracoclick.src="Baby-Draco-happy.gif";
+}
